e60e8db4d8f0e36ae91b748c354d500b73baf632
MOMO Adrien Nickson <mani@ekwali.local>
2021-04-27 12:41:20 +0200

    customisation du guard avec tailwindcss

--- src/Security/FactCheckerAuthenticator.php
+++ src/Security/FactCheckerAuthenticator.php
@@ -96,8 +96,7 @@ class FactCheckerAuthenticator extends AbstractFormLoginAuthenticator implements
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('main'));
     }
 
     protected function getLoginUrl()
